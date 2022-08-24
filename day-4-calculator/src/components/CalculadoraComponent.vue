5fbd9c7a90a90a20a8419e231bf321af80a88fb6
Laís Galvão <laisgbueno62@gmail.com>
2022-08-24 14:05:40 -0300

    dia 4 - calculadora

---
+++ day-4-calculator/src/components/CalculadoraComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="grid_buttons" :v-for="btn in buttons">
+    <button :v-model="btn.id">{{ btn.text }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    buttons() {
+      return [
+        {
+          id: 0,
+          text: "aa",
+        },
+        {
+          id: 1,
+          text: "aa",
+        },
+        {
+          id: 2,
+          text: "aa",
+        },
+        {
+          id: 3,
+          text: "aa",
+        },
+        {
+          id: 4,
+          text: "aa",
+        },
+      ];
+    },
+  },mounted(){
+    console.log(this.buttons.map(i => i.id));
+  }
+};
+</script>
+
+<style>
+.grid_buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  width: 300px;
+  margin: auto;
+}
+</style>
