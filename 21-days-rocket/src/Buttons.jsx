63e77b39e74f0df30809b4971fc2b66b0ec99160
Laís Galvão <laisgbueno62@gmail.com>
2022-09-01 10:38:22 -0300

    configs de meta tags do app  + listando os botões com um map()

--- 21-days-rocket/src/Buttons.jsx
+++ 21-days-rocket/src/Buttons.jsx
@@ -1,12 +1,44 @@
 import "./Buttons.css"
 
+let btns = [
+    {
+        id: 1,
+        txt: "Dia 1",
+        link: "https://github.com/LaisGalvao/21-days-of-code-rocketseat",
+        target: "_blank"
+    },
+    {
+        id: 2,
+        txt: "Dia 2",
+        link: "https://21-days-of-code-day2.netlify.app/",
+        target: "_blank"
+    },
+    {
+        id: 3,
+        txt: "Dia 3",
+        link: "https://21-days-of-code-day3.netlify.app/",
+        target: "_blank"
+    },
+    {
+        id: 4,
+        txt: "Dia 4",
+        link: "",
+        target: ""
+    }
+]
+
+let listBtn = btns.map((b, index) => {
+    return (<button key={index} className="btn-day">
+        <a href={b.link} target={b.target}>
+            <code>{b.txt}</code>
+        </a>
+    </button>)
+})
+
 export function Buttons() {
     return (
         <div className="btn-wrapper">
-            <button className="btn-day" ><code>Dia 1</code></button>
-            <button className="btn-day" ><code>Dia 2</code></button>
-            <button className="btn-day" ><code>Dia 3</code></button>
-            <button className="btn-day" ><code>Dia 4</code></button>
+            {listBtn}
         </div>
     )
 }
