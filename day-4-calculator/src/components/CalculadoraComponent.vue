cda7ee4cf87b3acdb6fa177ebc2b44c93ffec77a
Laís Galvão <laisgbueno62@gmail.com>
2022-09-01 16:55:13 -0300

    layout da calculadora

--- day-4-calculator/src/components/CalculadoraComponent.vue
+++ day-4-calculator/src/components/CalculadoraComponent.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="grid_buttons">
-    <div v-for="(btn, index) in buttons" :key="index">
-      <button class="btn" :id="btn.id">{{ btn.text }}</button>
+  <div class="grid">
+    <div class="grid_buttons">
+      <div v-for="(btn, index) in buttons_actions" :key="index">
+        <button @click="clicked(btn.text)" class="btn" :id="btn.id">
+          {{ btn.text }}
+        </button>
+      </div>
+    </div>
+    <div class="grid_buttons">
+      <div v-for="(btn, index) in buttons" :key="index">
+        <button @click="clicked(btn.text)" class="btn" :id="btn.id">
+          {{ btn.text }}
+        </button>
+      </div>
+    </div>
+    <div class="grid_buttons_operator">
+      <div v-for="(btn, index) in buttons_operations" :key="index">
+        <button @click="clicked(btn.text)" class="btn" :id="btn.id">
+          {{ btn.text }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -12,16 +30,16 @@ export default {
     return {
       buttons: [
         {
-          id: 0,
-          text: "1",
+          id: 6,
+          text: "7",
         },
         {
-          id: 1,
-          text: "2",
+          id: 7,
+          text: "8",
         },
         {
-          id: 2,
-          text: "3",
+          id: 8,
+          text: "9",
         },
         {
           id: 3,
@@ -36,20 +54,80 @@ export default {
           text: "6",
         },
         {
-          id: 6,
-          text: "7",
+          id: 0,
+          text: "1",
         },
         {
-          id: 7,
-          text: "8",
+          id: 1,
+          text: "2",
         },
         {
-          id: 8,
-          text: "9",
+          id: 2,
+          text: "3",
         },
         {
           id: 9,
-          text: "10",
+          text: "+/-",
+        },
+        {
+          id: 10,
+          text: "0",
+        },
+        {
+          id: 11,
+          text: ","
+        }
+      ],
+      buttons_actions: [
+        {
+          id: 0,
+          text: "%",
+        },
+        {
+          id: 1,
+          text: "CE",
+        },
+        {
+          id: 2,
+          text: "C",
+        },
+        {
+          id: 3,
+          text: "⅟x",
+        },
+        {
+          id: 4,
+          text: "x²",
+        },
+        {
+          id: 5,
+          text: "√ ",
+        },
+      ],
+      buttons_operations: [
+        {
+          id: 0,
+          text: "🧹",
+        },
+        {
+          id: 1,
+          text: "÷",
+        },
+        {
+          id: 2,
+          text: "×",
+        },
+        {
+          id: 3,
+          text: "-",
+        },
+        {
+          id: 4,
+          text: "+",
+        },
+        {
+          id: 5,
+          text: "=",
         },
       ],
     };
@@ -57,13 +135,37 @@ export default {
   mounted() {
     console.log(this.buttons.map((i) => i.id));
   },
+  methods: {
+    clicked(number) {
+      console.log(number);
+    },
+  },
 };
 </script>
 
 <style>
+.grid {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: stretch;
+  align-items: stretch;
+}
+
+.grid_buttons_operator {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  align-items: stretch;
+  margin-left: calc(100% - 75vw);
+  margin-top: -34.5%;
+}
+
 .grid_buttons {
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   align-content: center;
