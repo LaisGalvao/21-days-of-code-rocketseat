008394b666866fa46966660a2aab4a53d7faf3d9
Laís Galvão <laisgbueno62@gmail.com>
2022-09-06 18:50:55 -0300

    página de 404

---
+++ day-5-404-page/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Oops! Página não encontrada 🤔" />
+    <img src="./assets/dory-removebg-preview.png" alt="Dory" id="dory" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url(./assets/underwater.jpg);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img#dory {
+  animation-name: nadando;
+  animation-duration: 10s;
+  animation-delay: 3s;
+  animation-iteration-count: infinite;
+  position: relative;
+  left: -1200px;
+  overflow: hidden;
+}
+
+@keyframes nadando {
+  0% {
+    display: none;
+  }
+  25% {
+    display: block !important;
+    left: -52220px;
+  }
+  50% {
+    display: block !important;
+    left: 170px;
+  }
+  75% {
+    display: block !important;
+    left: 400px;
+  }
+  90%{
+    left: 500px;
+    overflow: hidden;
+  }
+  100% {
+    display: none;
+  }
+}
+</style>
---
+++ day-5-404-page/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  color: antiquewhite;
+  animation: fadeIn 5s;
+  -webkit-animation: fadeIn 5s;
+  -moz-animation: fadeIn 5s;
+  -o-animation: fadeIn 5s;
+  -ms-animation: fadeIn 5s;
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-moz-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-webkit-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-o-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-ms-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+</style>
