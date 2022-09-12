e5dde44606e0d0dd644a9945d999f5042bcee7f7
Laís Galvão <laisgbueno62@gmail.com>
2022-09-12 10:17:05 -0300

    dia 6 - card glassmorfismo

---
+++ day-6-glass-card/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="passe o mouse ou toque em mim! 🖱👆🏽 (se estiver no celular, toque dentro e fora da caixa)" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=100');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+}
+
+body::-webkit-scrollbar {
+  width: 9px;
+}
+body::-webkit-scrollbar-thumb {
+  background-color: darkslategrey;
+  border-radius: 20px;
+  border: 1px solid transparent;
+}
+ body::-webkit-scrollbar-track {
+  background: #00000047;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: whitesmoke;
+  margin: auto;
+ }
+</style>
---
+++ day-6-glass-card/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 id="message">{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  padding: 5%;
+width: 50vw;
+backdrop-filter: blur(2px) saturate(200%);
+  -webkit-backdrop-filter: blur(2px) saturate(200%);
+  background-color: rgba(17, 25, 40, 0.72);
+  border-radius: 12px;
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  transition: all 1s ease-in-out;
+}
+
+.hello:hover{
+  backdrop-filter: blur(15px) saturate(200%);
+    -webkit-backdrop-filter: blur(15px) saturate(200%);
+    background-color: rgba(0, 0, 0, 0.54);
+    border-radius: 12px;
+    border: 1px solid rgba(255, 255, 255, 0.125);
+    transform: scale(.99);
+    transition: all 1s ease-in-out;
+}
+
+@media screen and (max-width: 720px){
+  .hello{ width: auto;}
+}
+</style>
