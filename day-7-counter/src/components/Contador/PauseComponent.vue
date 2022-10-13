aeb8407787c3bc6fab698a9595f36ec55612f052
Laís Galvão <laisgbueno62@gmail.com>
2022-10-13 10:06:59 -0300

    criação do projeto + componentes do contador

---
+++ day-7-counter/src/components/Contador/PauseComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>pause</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ day-7-counter/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <ShowCountComponent/>
+      <StartComponent/>
+      <PauseComponent/>
+      <ResetComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import StartComponent from "./Contador/StartComponent.vue";
+import PauseComponent from "./Contador/PauseComponent.vue";
+import ResetComponent from "./Contador/ResetComponent.vue";
+import ShowCountComponent from "./Contador/ShowCountComponent.vue";
+export default {
+    name: "HelloWorld",
+    props: {
+        msg: String
+    },
+    components: { StartComponent, PauseComponent, ResetComponent, ShowCountComponent }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
